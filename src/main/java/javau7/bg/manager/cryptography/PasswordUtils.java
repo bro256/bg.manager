4b2ce6bae396f335f4f3de0d7a00fca6ca0c0bb9
Bronius <bronius256@gmail.com>
2023-12-06 09:15:01 +0200

    Generate salt from password exception handling

--- src/main/java/javau7/bg/manager/cryptography/PasswordUtils.java
+++ src/main/java/javau7/bg/manager/cryptography/PasswordUtils.java
@@ -5,8 +5,8 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class PasswordUtils {
-    public static byte[] generateSaltFromUsername(String username) throws NoSuchAlgorithmException {
-
+    public static byte[] generateSaltFromUsername(String username) {
+        try {
             MessageDigest digest = MessageDigest.getInstance(("SHA-256"));
             byte[] hashBytes = digest.digest(username.getBytes(StandardCharsets.UTF_8));
 
@@ -15,6 +15,8 @@ public class PasswordUtils {
             System.arraycopy(hashBytes, 0, salt, 0, 16);
 
             return salt;
-
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Error generating salt from username", e);
+        }
     }
 }
