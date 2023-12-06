579d5639e00cc63301aad866e99665d2224b5daa
Bronius <bronius256@gmail.com>
2023-12-06 09:12:18 +0200

    Generate salt from password method

---
+++ src/main/java/javau7/bg/manager/cryptography/PasswordUtils.java
@@ -0,0 +1,20 @@
+package javau7.bg.manager.cryptography;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class PasswordUtils {
+    public static byte[] generateSaltFromUsername(String username) throws NoSuchAlgorithmException {
+
+            MessageDigest digest = MessageDigest.getInstance(("SHA-256"));
+            byte[] hashBytes = digest.digest(username.getBytes(StandardCharsets.UTF_8));
+
+            // Taking 16 bytes (128 bits) as salt
+            byte[] salt = new byte[16];
+            System.arraycopy(hashBytes, 0, salt, 0, 16);
+
+            return salt;
+
+    }
+}
